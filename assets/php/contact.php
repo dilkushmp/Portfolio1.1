399152dfb25d2687d3be5e7d41f65cf12931609c
Mohammed Dilkush MP <62303291+dilkushmp@users.noreply.github.com>
2020-09-20 18:41:33 +0530

    Update contact.php

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,9 +1,19 @@
 <?php
+
+// Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	    echo "Post";
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+	    
 $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
 $field_message = $_POST['cf_message'];
 
-$mail_to = 'youremail@emaildomain.com';
+$mail_to = 'mpdilkush@gmail.com';
 $subject = 'Your Subject'.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
@@ -15,16 +25,13 @@ $headers .= 'Reply-To: '.$field_email."\r\n";
 
 $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
-if ($mail_status) { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Sucess Message');
-	</script>
-<?php
-}
-else { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Error Message');
-	</script>
-<?php
-}
+if ($mail_status) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thank You! Your message has been sent.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
 ?>
