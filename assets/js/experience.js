b3c057293f87c7259c0b9f254457151b62d6ab43
dilkush mp <mpdilkush@gmail.com>
2020-09-20 22:29:47 +0530

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -3,14 +3,14 @@
 const volunteership = document.querySelector(".volunteership");
 const volunteershipcards = [
   {
-    title:  "Institution of Engineers (India)"  ,
-    cardImage: "/assets/images/experience-page/ie.png",
+    title:  "Society of Automotive Engineers India ",
+    cardImage: "/assets/images/experience-page/sae.png",
     description:
       "First year: Student Representative, Second Year: General Secretary, Third Year: Student Convener ",
   },
   {
-    title:  "Society of Automotive Engineers India"  ,
-    cardImage: "/assets/images/experience-page/sae.png",
+    title:  "Institution of Engineers (India)",
+    cardImage: "/assets/images/experience-page/ie.png",
     description:
       "Particiapted SAE ADC 2020 as Team Captain of Aero NSS team",
   },
